e995dde4a694dbc37411d2af148df4b6c68aa81f
ShivamPatel-1802 <sp8487919091@gmail.com>
2026-02-16 14:31:41 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+        int a,b;
+        printf("\n Enter the value of A and B");
+        scanf("%d %d", &a ,&b);
+        printf("ADD=%d", a+b);
+        printf("SUB=%d", a-b);
+        printf("MULTIPLY=%d", a*b);
+        printf("DIVIDE=%d", a/b);
+        printf("REMAINDER=%d", a%b);
+        return 0;
+}
