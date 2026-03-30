3bc10f6868406f9934e5f02b953db9273900d1a4
PATEL SHIVAM <sp8487919091@gmail.com>
2026-03-30 10:03:32 +0530

    Create p3.c

---
+++ p3.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+// Recursive Function
+unsigned long long factorialRecursive(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorialRecursive(n - 1);
+}
+
+// Iterative Function
+unsigned long long factorialIterative(int n) {
+    unsigned long long fact = 1;
+    int i;
+
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main() {
+    int choice, num;
+
+    while(1) {
+        printf("\n===== Factorial Calculator =====\n");
+        printf("1. Factorial using Iterative Method\n");
+        printf("2. Factorial using Recursive Method\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if(choice == 3) {
+            printf("Program Exiting...\n");
+            break;
+        }
+
+        printf("Enter a number: ");
+        scanf("%d", &num);
+
+        // Edge Case Handling
+        if(num < 0) {
+            printf("Error: Factorial of negative numbers is not defined.\n");
+            continue;
+        }
+
+        if(num > 20) {
+            printf("Warning: Number too large. Result may overflow.\n");
+        }
+
+        switch(choice) {
+            case 1:
+                printf("Factorial (Iterative) = %llu\n", factorialIterative(num));
+                break;
+
+            case 2:
+                printf("Factorial (Recursive) = %llu\n", factorialRecursive(num));
+                break;
+
+            default:
+                printf("Invalid Choice! Please select 1, 2, or 3.\n");
+        }
+    }
+
+    return 0;
+}
