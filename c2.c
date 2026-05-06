c2d0c220e4660227e1f0765f1f0ae17b43bcd7b6
PATEL SHIVAM <sp8487919091@gmail.com>
2026-05-06 05:00:32 -0400

    Create c2.c

---
+++ c2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, a, b, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    a = 0;
+    b = 1;
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
